2e5789685d36a5e193953e4e0e30b968eedba185
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-05-26 12:54:44 +0700

    dependency injection

--- app/src/main/java/com/apps/sejarah/ui/main/MainActivity.kt
+++ app/src/main/java/com/apps/sejarah/ui/main/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.navigation.ui.setupWithNavController
 import com.apps.sejarah.R
 import com.apps.sejarah.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
