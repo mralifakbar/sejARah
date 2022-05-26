ecf8983440e734b9f30cac71d6cab2ffbcf0ca62
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-05-26 12:56:13 +0700

    add some constant values to constants class

---
+++ app/src/main/java/com/apps/sejarah/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.apps.sejarah.utils
+
+class Constants {
+    companion object{
+        const val PASSPHRASE = "aw_sejarah_26_mei"
+        const val DB_NAME = "sejarah_database_aw"
+    }
+}
